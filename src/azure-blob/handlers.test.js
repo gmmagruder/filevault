dffa14d06b2d8996d00389ee717d77a2d331a16e
gmmagruder <gabrielle.magruder@gmail.com>
2024-07-29 20:54:54 +0100

    Add unit tests

--- src/azure-blob/handlers.test.js
+++ src/azure-blob/handlers.test.js
@@ -1,7 +1,25 @@
-const express = require('express');
+process.env = {
+    AZURE_STORAGE_ACCOUNT_NAME: 'test name',
+    AZURE_STORAGE_ACCOUNT_KEY: 'test key',
+    AZURE_STORAGE_ACCOUNT_NAME: 'testurl'
+};
+
 const handlers = require('./handlers');
 
-jest.mock('@azure/storage-blob');
+jest.mock('@azure/storage-blob', () => ({
+    ...jest.requireActual('@azure/storage-blob'),
+    BlobServiceClient: jest.fn().mockImplementation(function () {
+        return {
+            getContainerClient: jest.fn().mockReturnValue({
+                getBlockBlobClient: jest.fn().mockReturnValue({
+                    uploadFile: jest.fn(),
+                    delete: jest.fn()
+                }),
+          }),
+        }
+    })
+}));
+
 jest.mock('mysql2/promise', () => ({
     createConnection: jest.fn().mockImplementation(function () {
         return {
@@ -11,8 +29,62 @@ jest.mock('mysql2/promise', () => ({
     })
 }));
 
+jest.mock('fs', () => ({
+    ...jest.requireActual('fs'),
+    unlinkSync: jest.fn(),
+}));
+
 describe('Test endpoints', () => {
-    const app = express();
+    it('getFiles fetches files successfully', async () => {
+        const req = {}
+        const res = {
+            json: jest.fn()
+        };
+
+        await handlers.getFiles(req, res);
+
+        expect(res.json).toHaveBeenCalled();
+    })
+
+    it('deleteFile fail to delete file', async () => {
+        const req = {params: {key: 1}}
+        const res = {
+            status: jest.fn().mockImplementation(function () {
+                return {send: jest.fn()}
+            }),
+        };
+
+        jest.spyOn(res, 'status').mockImplementationOnce(function () {
+            throw new Error("error");
+        })
+        
+        await handlers.deleteFile(req, res);
+        expect(res.status).toHaveBeenCalledWith(500);
+    });
+
+    it('deleteFile successfully', async () => {
+        const req = {params: {key: 1}}
+        const res = {
+            status: jest.fn().mockImplementation(function () {
+                return {send: jest.fn()}
+            }),
+        };
+        
+        await handlers.deleteFile(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
+
+    it('uploadFile successfully', async () => {
+        const req = {body: {note: 'test'}, file: {filename: 'test filename', path: 'test path'}}
+        const res = {
+            status: jest.fn().mockImplementation(function () {
+                return {send: jest.fn()}
+            }),
+        };
+        
+        await handlers.uploadFile(req, res);
+        expect(res.status).toHaveBeenCalledWith(200);
+    });
 
     it('uploadFile with no file name', async () => {
         const req = {body: {note: null}}
@@ -39,45 +111,18 @@ describe('Test endpoints', () => {
     });
 
     it('uploadFile fail to upload file', async () => {
-        const req = {body: {note: null}, file: true}
+        const req = {body: {note: "test"}, file: true}
         const res = {
             status: jest.fn().mockImplementation(function () {
                 return {send: jest.fn()}
             }),
         };
+
+        jest.spyOn(res, 'status').mockImplementationOnce(function () {
+            throw new Error("error");
+        })
         
         await handlers.uploadFile(req, res);
-        expect(res.status).toHaveBeenCalledWith(400);
+        expect(res.status).toHaveBeenCalledWith(500);
     });
-
-    // it('/upload successfully', async () => {
-    //     const req = {body: {note: null}, file: true}
-    //     const res = {
-    //         status: jest.fn().mockImplementation(function () {
-    //             return {send: jest.fn()}
-    //         }),
-    //     };
-
-    //     const mockQuery = jest.fn()
-    //     jest.mock('mysql2/promise', () => ({
-    //         createConnection: () => ({ 
-    //             connect: () => undefined,
-    //             query: mockQuery
-    //         }),
-    //     }))
-        
-    //     await handlers.uploadFile(req, res);
-    //     expect(res.status).toHaveBeenCalledWith(200);
-    // });
-
-    it('getFiles fetches files successfully', async () => {
-        const req = {}
-        const res = {
-            json: jest.fn()
-        };
-
-        await handlers.getFiles(req, res);
-
-        expect(res.json).toHaveBeenCalled();
-    })
   });
